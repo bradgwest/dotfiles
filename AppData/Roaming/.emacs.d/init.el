bf8aab82d88fcbcaa62ef6233d079e86fb2cab24
Brad West <brad.g.west@gmail.com>
2025-03-13 10:08:03 -0600

    [emacs] organize

--- AppData/Roaming/.emacs.d/init.el
+++ AppData/Roaming/.emacs.d/init.el
@@ -1,4 +1,4 @@
-;; TODO consider adding consult, embark
+;; TODO consider adding consult, embark, wgrep
 ;; TODO flyspell on org mode
 ;; TODO lsp for emacs lisp
 ;; TODO lsp for C#
@@ -9,25 +9,15 @@
 ;; TODO Occur mode for a project
 
 ;; Set garbage collection threshold higher during startup
+
+;; ### STARTUP ###
 (setq gc-cons-threshold (* 50 1024 1024))
 ;; Lower it after startup
 (add-hook 'after-init-hook
           (lambda ()
             (setq gc-cons-threshold (* 8 1024 1024))))
 
-(setq inhibit-startup-message t)
-
-(tool-bar-mode -1)         ; Disable the toolbar
-(menu-bar-mode -1)         ; Disable the menu bar
-(scroll-bar-mode -1)
-(fringe-mode 0)
-
-(setq mouse-wheel-tilt-scroll t)
-;; Prevent Extraneous Tabs
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-
-(set-face-attribute 'default nil :font "SauceCodePro NF" :height 95) ; height is in 1/10th of a pt
+;; ### PACKAGE MANAGEMENT ###
 
 ;; Initialize package sources
 (require 'package)
@@ -48,12 +38,23 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package whitespace
-  :config
-  (global-whitespace-mode -1)
-  :hook
-  ((yaml-mode
-    python-mode) . (lambda () (whitespace-mode t))))
+;; ### UI ###
+
+(setq inhibit-startup-message t)
+
+(tool-bar-mode -1)         ; Disable the toolbar
+(menu-bar-mode -1)         ; Disable the menu bar
+(scroll-bar-mode -1)
+(fringe-mode 0)
+
+(setq mouse-wheel-tilt-scroll t)
+;; Prevent Extraneous Tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(electric-indent-mode -1)
+
+;; needed to work with emacsclient
+(setq default-frame-alist '((font . "SauceCodePro NF-9.5")))
 
 (use-package doom-themes
   :custom
@@ -117,6 +118,41 @@
 (when (eq system-type 'windows-nt)
   (setq explicit-shell-file-name "C:/Program Files/PowerShell/7/pwsh.exe"))
 
+(use-package nerd-icons) ; (required for doom-modeline)
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(global-display-fill-column-indicator-mode t)
+
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 1)
+  (setq which-key-side-window-max-height 0.33)
+  (setq which-key-popup-type 'side-window)
+  (setq which-key-side-window-location 'bottom))
+
+(use-package company
+  :config
+  (global-company-mode 1)
+  (setq company-dabbrev-downcase nil))
+
+;; ### CODE ###
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
 ;; Install and configure powershell-mode
 (use-package powershell
   :mode ("\\.ps[1m]\\'" . powershell-mode))
@@ -129,52 +165,21 @@
             (lambda ()
               (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
 
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((powershell-mode . lsp)
+         ;;(csharp-mode . lsp)
+         ))
+
 ;; optionally
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
   :custom
   (lsp-ui-doc-position 'bottom))
 
-(use-package nerd-icons) ; (required for doom-modeline)
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-(column-number-mode)
-(global-display-line-numbers-mode t)
-(global-display-fill-column-indicator-mode t)
-
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; Do we need this?
-(use-package which-key
-  :defer 0
-  :diminish which-key-mode
-  :config (which-key-mode)
-  :config (setq which-key-idle-delay 1))
-
-(use-package company
-  :ensure t
-  :config
-  (global-company-mode 1)
-  (setq company-dabbrev-downcase nil))
-
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((powershell-mode . lsp)
-         ;; if you want which-key integration
-          (lsp-mode . lsp-enable-which-key-integration)))
-
 (use-package lsp-pyright
-  :ensure t
   :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
   :hook (python-mode . (lambda ()
                           (require 'lsp-pyright)
@@ -184,21 +189,12 @@
           (lambda ()
             (auto-revert-mode 1)))
 
-;; Huge pain on Windows.
-;; 1. First download hunspell
-;; 2. Figure out where hunspell stores dictionaries `hunspell -D`
-;; 2. Then download dictionaries to those locations:
-;;   Invoke-WebRequest -Uri https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.dic -OutFile "path/where/hunspell/looks/en_US.dic"
-;;   Invoke-WebRequest -Uri https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.aff -OutFile "path/where/hunspell/looks/en_US.aff"
-(use-package ispell
-  :defer t
-  :config
-  (setenv "DICTIONARY" "en_US")
-  (setq ispell-dictionary "en_US")
-  (setq ispell-program-name "hunspell")
-  ;; (setq ispell-local-dictionary "en_US")
-  (setq hunspell-default-dict "en_US")
-  (setq ispell-hunspell-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8))))
+(use-package csharp-mode)
+
+;; ### PROJECTS ###
+;; Need to install ripgrep: winget install --id BurntSushi.ripgrep.MSVC
+(use-package rg
+  :defer t)
 
 ;; Use project.el instead
 (use-package projectile
@@ -210,14 +206,21 @@
   (setq projectile-project-search-path '("D:/src"))
   (setq projectile-switch-project-action #'projectile-dired))
 
-;; Need to install ripgrep: winget install --id BurntSushi.ripgrep.MSVC
-(use-package rg
-  :defer t)
-
 (use-package projectile-ripgrep
   :defer t
   :after (rg projectile))
 
+(use-package whitespace
+   :config
+   (global-whitespace-mode -1)
+   :hook
+   ((yaml-mode
+     python-mode
+     csharp-mode
+     nxml-mode
+     emacs-lisp-mode) . (lambda () (whitespace-mode t))))
+
+;; ### DOCS ###
 (defun bgw/org-mode-setup ()
   (setq fill-column 120)
   (org-indent-mode)
@@ -235,7 +238,6 @@
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
-;; TODO put this in org section
 (require 'ox-md)
 
 (use-package plantuml-mode
@@ -249,7 +251,23 @@
   (plantuml-jar-path "C:/Users/bradwest/AppData/Roaming/PlantUML/plantuml-1.2024.5.jar")
   (plantuml-default-exec-mode 'jar))
 
-;; startup timing
+;; Huge pain on Windows.
+;; 1. First download hunspell
+;; 2. Figure out where hunspell stores dictionaries `hunspell -D`
+;; 2. Then download dictionaries to those locations:
+;;   Invoke-WebRequest -Uri https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.dic -OutFile "path/where/hunspell/looks/en_US.dic"
+;;   Invoke-WebRequest -Uri https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.aff -OutFile "path/where/hunspell/looks/en_US.aff"
+(use-package ispell
+  :defer t
+  :config
+  (setenv "DICTIONARY" "en_US")
+  (setq ispell-dictionary "en_US")
+  (setq ispell-program-name "hunspell")
+  ;; (setq ispell-local-dictionary "en_US")
+  (setq hunspell-default-dict "en_US")
+  (setq ispell-hunspell-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8))))
+
+;; ### STARTUP ###
 (defun bgw/display-startup-time ()
   (message "Emacs loaded in %s with %d garbage collections."
            (format "%.2f seconds"
@@ -257,6 +275,8 @@
                     (time-subtract after-init-time before-init-time)))
            gcs-done))
 (add-hook 'emacs-startup-hook #'bgw/display-startup-time)
+
+;; ### SYSTEM ADDED ###
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
